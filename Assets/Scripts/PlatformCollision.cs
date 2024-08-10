98b1f50503ca65cfc2e8f10d128d9311c7ceb10c
Anmol Mittal <anmolmittal1903@gmail.com>
2024-08-11 00:34:28 +0530

    One way platform

---
+++ Assets/Scripts/PlatformCollision.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+public class PlatformCollision : MonoBehaviour
+{
+    [SerializeField] GameObject oneWayCollider;
+    Collider2D playerCollider;
+
+    private void Awake()
+    {
+        playerCollider = GetComponent<Collider2D>();
+    }
+
+    public void DownPerformed()
+    {
+        if (oneWayCollider != null)
+        {
+            StartCoroutine(IgnoreCollisions());
+        }
+    }
+
+    IEnumerator IgnoreCollisions()
+    {
+        BoxCollider2D collider2D = oneWayCollider.GetComponent<BoxCollider2D>();
+
+        Physics2D.IgnoreCollision(playerCollider, collider2D);
+        yield return new WaitForSeconds(.25f);
+        Physics2D.IgnoreCollision(playerCollider, collider2D, false);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("OneWay"))
+        {
+            oneWayCollider = collision.gameObject;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("OneWay"))
+        {
+            oneWayCollider = null;
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,36 +5,80 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float jumpForce = 10f;
     [SerializeField] float groundCheckRadius = 0.2f;
-    [SerializeField] LayerMask groundLayer; 
-    [SerializeField] Transform groundCheck; 
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] Transform groundCheck;
 
     Rigidbody2D rb;
     bool isGrounded;
+    bool jumpRequest, downRequest;
+    float moveInput;
+
+    PlayerControls inputActions;
+
+    PlatformCollision platform;
 
     void Awake()
     {
+        inputActions = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        platform = GetComponent<PlatformCollision>();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+
+        inputActions.Movement.Horizontal.performed += Horizontal_performed;
+        inputActions.Movement.Jump.performed += Jump_performed;
+        inputActions.Movement.Down.performed += Down_performed;
+
+        inputActions.Movement.Horizontal.canceled += Horizontal_canceled;
+    }
+
+    private void Horizontal_canceled(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        moveInput = 0;
+    }
+
+    private void Down_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        if (isGrounded)
+            downRequest = true;
+    }
+
+    private void Jump_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        if (isGrounded)
+            jumpRequest = true;
+    }
+
+    private void Horizontal_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        moveInput = obj.ReadValue<float>();
     }
 
     void Update()
     {
-      
-        float moveInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
-        Debug.Log(moveInput);
-      
+
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
-        
-        if (Input.GetButtonDown("Jump") && isGrounded)
+
+        if (jumpRequest && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpRequest = false;
+        }
+
+        if (downRequest && isGrounded)
+        {
+            platform.DownPerformed();
+            downRequest = false;
         }
     }
 
     void OnDrawGizmosSelected()
     {
-        
         Gizmos.color = Color.red;
         Gizmos.DrawSphere(groundCheck.position, groundCheckRadius);
     }
